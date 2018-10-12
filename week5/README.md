da3ccf1344e36d5f00e6bdf3bb814cb889c4fdab
Nataly Klajner <klajn074@newschool.edu>
2018-10-12 02:53:39 +0000

    Add week 5

---
+++ week5/README.md
@@ -0,0 +1,13 @@
+# Assignment 5
+
+## Part 1
+In the sketches uploaded to week5 folder I tried to show my thoughts on how my entries for this assignments would be. 
+As they are thoughts that cross my mind there was no way to display them in a tabular way, for each entry has its own piece of information that was important for me at that moment. 
+
+## Part 2
+The code that was written uses a variable myItem to store the objects that contains strings for each entry. 
+With dynamodb.putItem the new item is being stored in the Dear Diary table created. 
+The last part of the code tells the console to log a success phrase.
+
+## Part 3
+For each entry the information was written in objetcs inside var myItem. As the dynamodb.putItem always calls for the same variable, for each new one that I wrote I commented out the previous entry. 
--- week5/week5.js
+++ week5/week5.js
@@ -6,18 +6,63 @@ AWS.config.region = "us-east-1";
 
 var dynamodb = new AWS.DynamoDB();
 
-var myItem = {
-  title: {
-    S: "My Second Post"
-  },
-  location: {
-    S: "Home"
-  },
-  pk: {
-    S: "oct09"
-  }
-};
+// var myItem = {
+//   title: {
+//     S: "I like the weather in NY"
+//   },
+//   location: {
+//     S: "Union Square"
+//   },
+//   pk: {
+//     S: "oct08"
+//   },
+//   time: {
+//     S: "3pm"
+//   },
+//   weatherNY: {
+//     S: "consistent each day"
+//   },
+//   weatherSP: {
+//     S: "full of surprises"
+//   },
+//   dontMiss: {
+//     SS: ["having to carry multiple jackets", "being hot/cold for wearing the wrong outfit"]
+//   }
+// };
 
+// var myItem = {
+//   title: {
+//     S: "Walking to school"
+//   },
+//   pk: {
+//     S: "oct10"
+//   },
+//   time: {
+//     S: "sometime in the morning"
+//   },
+//   commute: {
+//     S: "walking everyday for around 20 minutes to get to class"
+//   },
+//   thought: {
+//     S: "good way to start the day"
+//   },
+//   }
+  
+  var myItem = {
+    title: {
+      S: "My apartment"
+    },
+    location: {
+      S: "Home"
+    },
+    pk: {
+      S: "oct11"
+    },
+    skills: {
+      SS: ["learning how to properly clean a bathroom", "learning how to cook", "buying and fixing up furniture"]
+    },
+  }
+  
 dynamodb.putItem(
   { 
     TableName: "deardiary",
@@ -25,6 +70,10 @@ dynamodb.putItem(
   },
                  
 	function(error, data) {
-    console.log("Hey! I'm done");
+	  if (error) {
+	    console.log(error);
+	  } else {
+      console.log("Hey! I'm done"); 
+	  }
 	}
 );
