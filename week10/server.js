a23a043968c43ec4787801fd8ede995b0e29b2b1
Nataly Klajner <klajn074@newschool.edu>
2018-12-07 00:23:49 +0000

    Add week10

---
+++ week10/server.js
@@ -0,0 +1,107 @@
+var express = require('express'), // npm install express
+    app = express();
+const { Pool } = require('pg');
+var AWS = require('aws-sdk');
+
+// AWS RDS credentials
+var db_credentials = new Object();
+db_credentials.user = 'nataly';
+db_credentials.host = 'mydb.cq2lipog5ub4.us-east-1.rds.amazonaws.com';
+db_credentials.database = 'natalysdb';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+// AWS DynamoDB credentials
+AWS.config = new AWS.Config();
+AWS.config.accessKeyId = process.env.AWS_ID;
+AWS.config.secretAccessKey = process.env.AWS_KEY;
+AWS.config.region = "us-east-1";
+
+// respond to requests for /sensor
+app.get('/sensor', function(req, res) {
+    
+    // Connect to the AWS RDS Postgres database
+    const client = new Pool(db_credentials);
+
+    // SQL query 
+    var q = `SELECT EXTRACT(DAY FROM sensorTime) as sensorday,
+             AVG(sensorValue::int) as num_obs
+             FROM sensorData
+             GROUP BY sensorday
+             ORDER BY sensorday;`;
+
+    client.connect();
+    client.query(q, (qerr, qres) => {
+        if (qerr) { throw qerr }
+        else {
+            res.send(qres.rows);
+            client.end();
+            console.log('1) responded to request for sensor data');
+        }
+    });
+});
+
+// respond to requests for /aameetings
+app.get('/aameetings', function(req, res) {
+    
+    // Connect to the AWS RDS Postgres database
+    const client = new Pool(db_credentials);
+    
+    // SQL query 
+    // change FROM aadata (starter code) to FROM aameetings
+    // change mtglocation (starter code) to title (my database)
+    var thisQuery = `SELECT address, title as location, json_agg(json_build_object('day', day, 'time', startTime)) as meetings
+                 FROM aameetings 
+                 WHERE day = 'Sundays' and startTime = '6:00 PM' and wheelchairAccess = true
+                 GROUP BY address, title
+                 ;`;
+
+    client.query(thisQuery, (qerr, qres) => {
+        if (qerr) { throw qerr }
+        else {
+            res.send(qres.rows);
+            client.end();
+            console.log('2) responded to request for aa meeting data');
+        }
+    });
+});
+
+// respond to requests for /deardiary
+app.get('/deardiary', function(req, res) {
+
+    // Connect to the AWS DynamoDB database
+    var dynamodb = new AWS.DynamoDB();
+    
+    // DynamoDB (NoSQL) query
+    var params = {
+        TableName: "deardiary",
+        KeyConditionExpression: "#pk = :pk", // the query expression
+        ExpressionAttributeNames: { // name substitution, used for reserved words in DynamoDB
+        "#pk" : "pk"
+    },
+        ExpressionAttributeValues: { // the query values
+        ":pk": {S: "nov03"},
+        }
+    };
+    
+    
+
+    dynamodb.query(params, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        }
+        else {
+            res.send(data.Items);
+            console.log('3) responded to request for dear diary data');
+        }
+    });
+
+});
+
+// serve static files in /public
+app.use(express.static('public'));
+
+// listen on port 8080
+app.listen(8080, function() {
+    console.log('Server listening...');
+});
